3157b479a84912c5dfbd0837de20993822753541
Patricio Scelso <patricioscelso@gmail.com>
2020-06-25 11:54:19 -0300

    Falto commitear anoche

--- api/src/routes/products.js
+++ api/src/routes/products.js
@@ -15,25 +15,11 @@ server.get('/', function(req, res) {
        Product.findOne({where :{
            titulo: name,
        }}).then(function(products){
-        console.log(products);
+        
         return res.status(200).send(products)
        })
     })     
-// server.get('/:search', function (req, res){
-//     Product.findOne({
-//         where: {titulo : req.params.search
-//         }
-//     })
-//     .then(function(products) {
-//         if (products) {
-//         var err = new Error("El producto no fue encontrado");
-//         err.status = 404;
-//         throw err;
-//     }
-//             res.render('Product', { products })
-//         res.sendStatus(404);
-//       }); 
-// });
+
 
 
 
