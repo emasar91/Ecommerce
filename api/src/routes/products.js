4e78834cf221511db4f7b1f4f575ab7811de3c3f
Patricio Scelso <patricioscelso@gmail.com>
2020-06-24 21:23:41 -0300

    Agregada ruta de filtrado

--- api/src/routes/products.js
+++ api/src/routes/products.js
@@ -4,10 +4,38 @@ const {Product} = require('../models');
 
 server.get('/', function(req, res) {
     Product.findAll()
-        .then(function(product) {
-            res.send(product);
+        .then(function(products) {
+
+            return res.status(200).send(products);
         });
 });
+
+  server.get('/:search', function (req, res) {
+      var name = req.params.search;
+       Product.findOne({where :{
+           titulo: name,
+       }}).then(function(products){
+        console.log(products);
+        return res.status(200).send(products)
+       })
+    })     
+// server.get('/:search', function (req, res){
+//     Product.findOne({
+//         where: {titulo : req.params.search
+//         }
+//     })
+//     .then(function(products) {
+//         if (products) {
+//         var err = new Error("El producto no fue encontrado");
+//         err.status = 404;
+//         throw err;
+//     }
+//             res.render('Product', { products })
+//         res.sendStatus(404);
+//       }); 
+// });
+
+
 
 
 module.exports = server;
