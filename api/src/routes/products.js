e825f0bc3dc9002bfc15a0b022f4c9f4bc2761dc
kelm2020 <kelmchantalbelen@gmail.com>
2020-06-25 21:47:42 -0300

    ID y SEARCH HECHOS

--- api/src/routes/products.js
+++ api/src/routes/products.js
@@ -1,39 +1,75 @@
 const server = require('express').Router();
-const {Product} = require('../models');
+const { Product } = require('../models');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 
 server.get('/', function(req, res) {
     Product.findAll()
         .then(function(products) {
-
             return res.status(200).send(products);
         });
 });
 
-  server.get('/:search', function (req, res) {
-      var name = req.params.search;
-       Product.findOne({where :{
-           titulo: name,
-       }}).then(function(products){
+server.get('/search/:search', function(req, res) {
+    var name = req.params.search;
+    Product.findAll({
+        where: {
+            titulo: {
+                [Op.like]: '%' + name + '%'
+            }
+        }
+
+
+    }).then(function(products) {
         console.log(products);
         return res.status(200).send(products)
-       })
-    })     
-// server.get('/:search', function (req, res){
-//     Product.findOne({
-//         where: {titulo : req.params.search
-//         }
-//     })
-//     .then(function(products) {
-//         if (products) {
-//         var err = new Error("El producto no fue encontrado");
-//         err.status = 404;
-//         throw err;
-//     }
-//             res.render('Product', { products })
-//         res.sendStatus(404);
-//       }); 
-// });
+    })
+})
+
+server.get('/:id', function(req, res) {
+    var numero = req.params.id;
+    Product.findOne({
+        where: {
+            id: numero,
+        }
+    }).then(function(products) {
+        console.log(products);
+        return res.status(200).send(products)
+    })
+})
+
+
+
+server.post('/agregar', function(req, res) {
+    Product.create({
+        titulo: req.body.titulo,
+        precio: req.body.precio,
+        cantidad: req.body.cantidad,
+        imagen: req.body.imagen,
+        descripcion: req.body.descripcion,
+    });
+    res.send('Se ha agregado un nuevo producto');
+
+});
+
+server.put('/modificar/:id', function(req, res) {
+    Product.findOne({
+        where: {
+            id: req.params.id,
+        }
+    }).then(function(product) {
+        product.update({
+            titulo: req.body.titulo,
+            precio: req.body.precio,
+            cantidad: req.body.cantidad,
+            imagen: req.body.imagen,
+            descripcion: req.body.descripcion,
+        })
+    })
+    res.send('Se ha modificado el producto');
+});
+
 
 
 
