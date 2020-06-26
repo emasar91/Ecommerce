e57a4a22d50ffb9af67780ff2d25d96454347875
kelm2020 <kelmchantalbelen@gmail.com>
2020-06-26 15:39:07 -0300

    products listos

--- api/src/routes/products.js
+++ api/src/routes/products.js
@@ -15,7 +15,7 @@ server.get('/search/:search', function(req, res) {
     Product.findAll({
         where: {
             titulo: {
-                [Op.like]: '%' + name + '%'
+                [Op.iLike]: '%' + name + '%'
             }
         }
 
@@ -51,24 +51,52 @@ server.post('/agregar', function(req, res) {
 
 });
 
-server.put('/modificar/:id', function(req, res) {
-    Product.findOne({
-        where: {
-            id: req.params.id,
-        }
-    }).then(function(product) {
-        product.update({
-            titulo: req.body.titulo,
-            precio: req.body.precio,
-            cantidad: req.body.cantidad,
-            imagen: req.body.imagen,
-            descripcion: req.body.descripcion,
-        })
-    })
-    res.send('Se ha modificado el producto');
+server.put("/:productId", function(req, res) {
+    var prod = function() {
+        return Product.findByPk(req.params.productId);
+    };
+    var cat = function() {
+        return Category.findOne({
+            where: {
+                categoryName: req.body.categoryName
+            }
+        });
+    };
+    if (req.body.accion === "add") {
+        Promise.all([prod(), cat()])
+            .then((response) => {
+                if (response[0] && response[1]) {
+                    response[0].addCategory(response[1]);
+                    res.send("Succesfull add");
+                } else {
+                    res.status(404).send("La categoria o el producto no existe");
+                };
+            }).catch(() => res.sendStatus(400));
+    } else if (req.body.accion === "remove") {
+        Promise.all([prod(), cat()])
+            .then((response) => {
+                if (response[0] && response[1]) {
+                    response[0].removeCategory(response[1]);
+                    res.send("Succesfull remove");
+                } else {
+                    res.status(404).send("La categoria o el producto no existe");
+                };
+            }).catch(() => res.sendStatus(400));
+    } else {
+        res.status(400).send("La accion debe existir y solo puede ser add o remove");
+    };
 });
 
-
+server.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    Product.destroy({
+            where: { id: id },
+        })
+        .then(deletedProduct => {
+            res.json(deletedProduct);
+        })
+        .catch(res.send);
+});
 
 
 
