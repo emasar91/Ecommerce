c4ca098911a555688349ece8b9e6541d466ff865
kelm2020 <kelmchantalbelen@gmail.com>
2020-06-26 16:00:44 -0300

    category HECHO

--- api/src/models/Category.js
+++ api/src/models/Category.js
@@ -18,33 +18,6 @@ const Category = (sequelize, S) => {
     return C;
 };
 
-server.put('/:id', (req, res) => {
-    const id = req.params.id;
-
-    category.update(req.body, {
-            where: {
-                id: id,
-            },
-            returning: true,
-        })
-        .then(response => {
-            const category = response[1][0];
-            return category;
-        })
-        .then(category => res.send(category))
-        .catch(err => res.send(err.message));
-});
-
-server.delete('/:id', (req, res) => {
-    const id = req.params.id;
-    category.destroy({
-            where: { id: id },
-        })
-        .then(deletedCategory => {
-            res.json(deletedCategory);
-        })
-        .catch(res.send);
-});
 
 
 module.exports = Category;
