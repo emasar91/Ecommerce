7c967075b8e18ca8b88e52c37258b6f4a3fdf1d3
Patricio Scelso <patricioscelso@gmail.com>
2020-07-02 16:03:04 -0300

    jueves

--- api/src/models/Orden.js
+++ api/src/models/Orden.js
@@ -2,7 +2,7 @@ const S = require('sequelize');
 const Orden = (sequelize, S) => {
     // defino el modelo
 
-    const O = sequelize.define('user', {
+    const O = sequelize.define('orden', {
             idOrden: {
                 primaryKey: true,
                 type: S.INTEGER,
---
+++ api/src/routes/ordenes.js
@@ -0,0 +1,13 @@
+const server = require('express').Router();
+const { Orden } = require('../models');
+const Sequelize = require('sequelize');
+
+
+server.post('/')
+
+
+
+
+
+
+module.exports = server;
