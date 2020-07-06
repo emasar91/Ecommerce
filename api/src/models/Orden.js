9ffa7a4c370d36db480a9d588a29d9ad852c6ed5
kelm2020 <kelmchantalbelen@gmail.com>
2020-07-06 13:35:52 -0300

    cambios SPRINT2

--- api/src/models/Orden.js
+++ api/src/models/Orden.js
@@ -1,20 +1,52 @@
 const S = require('sequelize');
-const Orden = (sequelize, S) => {
-    // defino el modelo
+const Product = require('./Product');
+// const Orden = (sequelize, S) => {
+//     // defino el modelo
 
-    const O = sequelize.define('orden', {
+//     const O = sequelize.define('orden', {
+//         idOrden: {
+//             primaryKey: true,
+//             type: S.INTEGER,
+//             allowNull: false,
+//             autoIncrement: true,
+//         },
+//         estado: {
+//             type: S.STRING,
+//             defaultValue: 'abierto',
+//         },
+//     }, { timestamps: false });
+
+//     return O;
+// };
+const Productoxorden = (sequelize, S) => {
+    const C = sequelize.define("productoxorden", {
+        cantidad: {
+            type: S.INTEGER,
+            allowNull: true,
+        },
+        precioVenta: {
+            type: S.INTEGER,
+            allowNull: true
+        }
+    });
+    return C;
+};
+module.exports = Productoxorden;
+
+const Orden = (sequelize, S) => {
+    const O = sequelize.define("orden", {
         idOrden: {
-            primaryKey: true,
             type: S.INTEGER,
             allowNull: false,
             autoIncrement: true,
+            unique: true,
+            primaryKey: true,
         },
         estado: {
             type: S.STRING,
-            defaultValue: 'abierto',
-        },
-    }, { timestamps: false });
-
+            allowNull: false
+        }
+    });
     return O;
 };
 
